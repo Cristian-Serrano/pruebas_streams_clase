697b22bfe3cad893b5b57ddadea7cc3ad65675db
Cristian-Serrano <gcristiag@gmail.com>
2023-10-13 12:40:53 +0200

    terminado

---
+++ src/test/java/org/iesvdm/StreamTest.java
@@ -0,0 +1,46 @@
+package org.iesvdm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class StreamTest {
+    List<Dish> menu;
+    @BeforeEach
+    void setUp(){
+        this.menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH) );
+    }
+
+    @Test
+    public void streamTest(){
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+
+        for (Dish dish : menu) {
+            if (dish.getCalories() < 400) {
+                lowCaloricDishes.add(dish);
+            }
+        }
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            public int compare(Dish dish1, Dish dish2) {
+                return Integer.compare(dish1.getCalories(), dish2.getCalories());
+            }
+        });
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        for (Dish dish : lowCaloricDishes) {
+            lowCaloricDishesName.add(dish.getName());
+        }
+
+        Assertions.assertTrue(true);
+    }
+}
